40a6acef43d4af29f751b44a91e8460ca2b50ae0
Joe Wickes <joe.c.wickes@gmail.com>
2021-02-22 16:06:14 -0500

    Update for heroku deploy issue

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,7 +5,7 @@ module.exports = {
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    : `${process.env.DATABASE_URL}?ssl=true`,
   // "host": process.env.MIGRATION_DATABASE_HOST,
   // "port": process.env.MIGRATION_DATABASE_PORT,
   // "database": process.env.MIGRATION_DATABASE_NAME,
